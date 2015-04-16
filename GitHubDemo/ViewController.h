a0a87889b13c51187b82849b5b01675603764a23
jdlace <jlace@shp.org>
2015-04-15 21:04:37 -0400

    Initial Commit

---
+++ GitHubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubDemo
+//
+//  Created by Jonathan Lace on 4/15/15.
+//  Copyright (c) 2015 SHP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHubDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitHubDemo
+//
+//  Created by Jonathan Lace on 4/15/15.
+//  Copyright (c) 2015 SHP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
